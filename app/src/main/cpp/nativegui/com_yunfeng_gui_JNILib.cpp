3a1248b3ed0a777a4a9e56313dd5db37d6294fb0
xiayunfeng <xiayunfeng2012@gmail.com>
2018-10-09 14:48:28 +0800

    update render name

--- app/src/main/cpp/nativegui/com_yunfeng_gui_JNILib.cpp
+++ app/src/main/cpp/nativegui/com_yunfeng_gui_JNILib.cpp
@@ -1,7 +1,7 @@
 #include <android/asset_manager_jni.h>
 //#include <android/ndk-version.h>
 #include "com_yunfeng_gui_JNILib.h"
-#include "renderer/Renderer.h"
+#include "renderer/GLESRenderer.h"
 #include "renderer/VulkanRenderer.h"
 #include "renderer/utils.h"
 
@@ -24,17 +24,17 @@ JNIEXPORT void JNI_OnUnload(JavaVM *, void *) {
 JNIEXPORT void JNICALL Java_com_yunfeng_guinative_JNILib_nativeOnSurfaceCreated
         (JNIEnv *env, jclass, jobject assetManager) {
     AAssetManager *aAssetManager = AAssetManager_fromJava(env, assetManager);
-    Renderer::getInstance()->init(aAssetManager);
+    GLESRenderer::getInstance()->init(aAssetManager);
 }
 
 JNIEXPORT void JNICALL Java_com_yunfeng_guinative_JNILib_nativeOnSurfaceChanged
         (JNIEnv *, jclass, jint width, jint height) {
-    Renderer::getInstance()->onChanged(width, height);
+    GLESRenderer::getInstance()->onChanged(width, height);
 }
 
 JNIEXPORT void JNICALL Java_com_yunfeng_guinative_JNILib_nativeOnDrawFrame
         (JNIEnv *, jclass) {
-    Renderer::getInstance()->onDrawFrame();
+    GLESRenderer::getInstance()->onDrawFrame();
 }
 
 #ifdef __cplusplus
